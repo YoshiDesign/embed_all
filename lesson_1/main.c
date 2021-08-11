8e0f729a2cbf834a25d10270994e507d16ee7c9d
YoshiDesign <30674964+YoshiDesign@users.noreply.github.com>
2021-08-11 01:01:56 -0400

    init

---
+++ lesson_1/main.c
@@ -0,0 +1,18 @@
+
+int main()
+{
+
+    unsigned int f, u, q;
+    f = u = q = 0;
+    ++f;
+    ++f;
+    ++f;
+    ++f;
+    ++f;
+    ++f;
+    ++f;
+    ++f; 
+    ++u; 
+    ++q;
+    return 0;
+}
